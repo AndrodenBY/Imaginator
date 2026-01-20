50913edb580c9e90f7b4acc4548a36e333289e97
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-01-20 10:23:41 +0100

    ImaginatorV1

---
+++ Constants.cs
@@ -0,0 +1,12 @@
+namespace Imaginator;
+
+public static class Constants
+{
+    public const int Channels = 3;
+    public const int IndexOffset = 1;
+    public const int MaxByteValue = 255;
+    public const double HeightAspectDivisor = 2.0;
+    public const string ErrorMessage = "ERROR LOADING IMAGE";
+    public const string AsciiSymbols = " .:-=+*#%@";
+    //public const string AsciiSymbols = " .:-=+*#%@$&WM8";
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿while (true)
+{
+    Console.Write("Write filepath: ");
+    string? path = Console.ReadLine();
+    
+    if (String.IsNullOrWhiteSpace(path))
+    {
+        continue;
+    }
+    
+    Imaginator.AsciiMilator.Imaginate(path);
+    Console.WriteLine("Press Control+C to exit...");
+}
